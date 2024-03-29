fae787f8eaad41b0b2f68d9d2715fa208d803f33
Vincent Chi <vincent.t.chi@gmail.com>
2024-03-29 10:30:19 +0000

    Areef - implemented the main menu and a controller to go to different menus

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import csv
 from user import User
+from user import Create_User
 
 class Main():
-  
-  def main_menu(self):
-    exit=0
-    self.login_logout()
-    while exit !=0:
-      print("----Welcome to the main menu---")
-      print("0. Create New User ")
-      print("1.) Logout")
-      print("2.) Enter Item Interface  ")
-      print("3.) Enter User Interface  ")
-      print("4.) Enter Category Interface")
-      print("5.) Enter Display Interface")
-      print("6.) Enter Sort Interface")
-      print("7.) Enter Search Interface")
-      print("8.) Enter Stock-Alert Interface")
-      # user_input = input("Enter input")
-      
+    
+    def remote(self,option):
+        if option == -10:
+            print("--Login/Logout Interface--")
+        elif option == 1: 
+            print("--Create new user Interface--")
+        elif option == 2: 
+            print("--Create Item Interface--")
+        elif option == 3:
+            print("--Create Category Interface--")
+        elif option == 4: 
+            print("--Create Search Interface--")
+
+    
+    def create_user(self):
+        print("Enter your login credential:\n")
+        create_username = input("Create Username: ")
+        create_password = input("Create Password: ")
+        user1 = Create_User(create_username, create_password)
+
+    
+    def login(self):
+        login_username = input("Login Username: ")
+        login_password = input("Login Password: ")
+        logged_user = User(login_username, login_password)    
+        print("message after login sucessful, this line should not print if login has failed")
+
+    
+    def main_menu(self):
+        user_input=-1
+        while exit !=0:
+            print("------ Main Menu ------")
+            print("1. Create New User ")
+            print("2.) Enter Item Interface  ")
+            print("3.) Enter Category Interface")
+            print("4.) Search")
+            print("5.) Logout and Exit")
+            
+            user_input = int(input("Enter your choice (0-5):"))
+            print("\n")
+            if user_input == 5:
+                print("Successfully Logged Out Of System ")
+                break
+            self.remote(user_input)
     
   # def access_item_category_data():
   #     with open('data.csv', mode ='r') as file:
@@ -26,18 +54,5 @@ class Main():
   #     for lines in csvFile:
   #           print(lines)
 
-  #def create_user(self):
-  #  create_username = input("Create Username: ")
-  #  create_password = input("Create Password: ")
-  #  user1 = Create_User(create_username, create_password)
 
-  #def login(self):
-  #  login_username = input("Login Username: ")
-  #  login_password = input("Login Password: ")
-  #  logged_user = User(login_username, login_password)    
-  #  print("message after login sucessful, this line should not print if login has failed")
-
-
-    
-    
 Main().main_menu()
