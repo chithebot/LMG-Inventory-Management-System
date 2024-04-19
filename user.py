4b3feefe18186d0b71fe306a6621db33598420d2
Vincent Chi <vincent.t.chi@gmail.com>
2024-04-19 23:08:50 +0000

    Revert to old project design; making new branch for new project design

--- user.py
+++ user.py
@@ -1,78 +1,33 @@
-import entity
+#user.py
+#Hao Cui has claimed this spot. Do not touch this file
+import csv
+import sys
 
+class Create_User:
 
-class User(entity.Entity):
-    """ This class represents a user.
+  #user createion
+  def __init__(self, user_name, user_password):
+    self.create_user_name = user_name.lower()
+    self.create_password = user_password
+    with open("user.csv") as file:
+      if self.create_user_name in file.read(): #check if username is in the file
+        print(f"The username {self.create_user_name} already exists!")
+      else:
+        with open("user.csv", mode = "a") as file:
+          writer = csv.writer(file)
+          writer.writerow([self.create_user_name , self.create_password])
+          print("Your Account has been created successfully!")
+          print(f"Your Username is: {self.create_user_name} \nYour Password is: {self.create_password}")
 
-        Attributes:
-            _username (string)  : the user's username
-            _password (string)  : the user's password
-    """
-    __doc__ += ('\n' + entity.Entity.__doc__)
-
-
-    """
-    This function initializes an User object.
-
-    Args:
-        self (User)		    : the current User object
-        name (string)	    : the name of the User object
-        username (string)	: the username of the User object
-        password (string)	: the password of the User object
-    """ 
-    def __init__(self, name="", username=""):
-        super().__init__(name)
-        self._username = username
-
-
-    """
-    This function gets the User object's username.
-
-    Args:
-        self (User) : the current User object
-    
-    Returns:
-        Returns the User object's username.
-    """ 
-    @property
-    def username(self):
-        return self._username
-
-
-    """
-    This function sets the User object's username.
-
-    Args:
-        self (User)		    : the current User object
-        username (string)	: the new username to set
-    """ 
-    @username.setter
-    def username(self, username):
-        self._username = username
-
-
-    """
-    This function sets all of the current User object's
-    attributes to empty strings, or clears them.
-
-    Args:
-        self (User)		    : the current User object
-        username (string)	: the new username to set
-    """ 
-    def clear(self):
-        self.username = str()
-        self.name = str()
-
-
-    """
-    This function returns the string representation of the current
-    User object.
-
-    Args:
-        self (User)	: the current User object
-
-    Returns:
-        Returns a string which represents the current User object.
-    """
-    def __str__(self):
-        return super().__str__() + "\t" + self.username
+class User:
+  def __init__(self, user_name, user_password):
+    with open("user.csv") as file:
+      account = [user_name, user_password]
+      reader = csv.reader(file)
+      for i in reader:
+        if user_name in i and account == i:
+          print("Login successful!")
+          break
+        else:
+          print("Login Failed!")
+          sys.exit(1)
